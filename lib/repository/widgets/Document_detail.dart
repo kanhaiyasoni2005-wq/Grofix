6b3297f8dbe76f552697ba1c268cf553d07167df
Kanhaiya Soni <kanhaiyasoni2005@gmail.com>
2026-06-01 14:03:58 +0530

    grofix site open in borwser

--- lib/repository/widgets/Document_detail.dart
+++ lib/repository/widgets/Document_detail.dart
@@ -1,60 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class DocumentDetail extends StatefulWidget {
+class DocumentDetail extends StatelessWidget {
   const DocumentDetail({super.key});
 
-  @override
-  State<DocumentDetail> createState() => _DocumentDetailState();
-}
+  Future<void> openWebsite() async {
+    final Uri url =
+        Uri.parse("https://sites.google.com/view/grofix/documents");
 
-class _DocumentDetailState extends State<DocumentDetail> {
-  InAppWebViewController? webViewController;
-  bool isLoading = true;
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Document Details"),
-      ),
-      body: Stack(
-        children: [
-          InAppWebView(
-            initialUrlRequest: URLRequest(
-              url: WebUri(
-                "https://sites.google.com/view/grofix/documents",
-              ),
-            ),
-            initialSettings: InAppWebViewSettings(
-              javaScriptEnabled: true,
-              useShouldOverrideUrlLoading: true,
-              mediaPlaybackRequiresUserGesture: false,
-              supportZoom: true,
-              useHybridComposition: true,
-            ),
-            onWebViewCreated: (controller) {
-              webViewController = controller;
-            },
-            shouldOverrideUrlLoading:
-                (controller, navigationAction) async {
-              return NavigationActionPolicy.ALLOW;
-            },
-            onLoadStop: (controller, url) {
-              setState(() {
-                isLoading = false;
-              });
-            },
-            onReceivedError: (controller, request, error) {
-              debugPrint("WebView Error: ${error.description}");
-            },
-          ),
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      openWebsite();
+      Navigator.pop(context); // optional
+    });
 
-          if (isLoading)
-            const Center(
-              child: CircularProgressIndicator(),
-            ),
-        ],
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
       ),
     );
   }
